f55482c079d5dda37063811f33a6800cbd856384
sajid <152436427+sajidbaba1@users.noreply.github.com>
2025-07-31 01:32:16 +0530

    initial commit

--- src/main/java/com/lookhook/FashionStoreLookbookApplication.java
+++ src/main/java/com/lookhook/FashionStoreLookbookApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FashionStoreLookbookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FashionStoreLookbookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FashionStoreLookbookApplication.class, args);
+    }
 
 }
--- src/test/java/com/lookhook/FashionStoreLookbookApplicationTests.java
+++ src/test/java/com/lookhook/FashionStoreLookbookApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FashionStoreLookbookApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
